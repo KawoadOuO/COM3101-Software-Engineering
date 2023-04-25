3ef93a9067e6be66aaf77ee69fe9185c097e1bc1
KawoadOuO <113492652+KawoadOuO@users.noreply.github.com>
2023-04-25 22:31:35 +0800

    3(2)

--- src/hsuadddrop/HSUsystemMain.java
+++ src/hsuadddrop/HSUsystemMain.java
@@ -4,7 +4,8 @@ import hsuadddrop.model.Model;
 public class HSUsystemMain {
     Controller Controller = new Controller();
     Model Model = new Model();
+    public static void main(String[] args) {
     
-    
-    
+    System.out.println("Hello World!");
+    }
 }
--- src/hsuadddrop/model/Model.java
+++ src/hsuadddrop/model/Model.java
@@ -1,5 +1,14 @@
 package hsuadddrop.model;
+import hsuadddrop.Controller;
+import hsuadddrop.view.MainUI;
 
 public class Model {
-    
+    Model model;
+    MainUI view;
+    public void setModel(Model m) {
+            this.model = m;        
+       }
+    public void setView(MainUI v) {
+            this.view = v;        
+       }
 }
